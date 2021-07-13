89e785586210be408d43f6b0826424bd5f2c97d4
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2021-07-13 18:03:48 +0700

    add solver

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,6 @@
 
 
-testGrid2 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+solveGrid = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
              [6, 7, 2, 1, 9, 5, 3, 4, 8],
              [1, 9, 8, 3, 4, 2, 5, 6, 7],
              [8, 5, 9, 7, 6, 1, 4, 2, 3],
@@ -9,6 +9,17 @@ testGrid2 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
              [9, 6, 1, 5, 3, 7, 2, 8, 4],
              [2, 8, 7, 4, 1, 9, 6, 3, 5],
              [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+
+testGrid = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+             [6, 0, 2, 1, 9, 5, 3, 4, 8],
+             [1, 9, 8, 3, 4, 2, 5, 0, 7],
+             [8, 5, 9, 7, 6, 1, 4, 2, 3],
+             [4, 2, 6, 0, 5, 3, 7, 9, 1],
+             [7, 1, 3, 9, 2, 4, 8, 5, 6],
+             [9, 6, 1, 5, 3, 7, 2, 8, 4],
+             [2, 8, 7, 4, 1, 9, 6, 0, 5],
+             [3, 4, 5, 2, 8, 6, 1, 0, 9]]
 
 grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
         [5, 2, 0, 0, 0, 0, 0, 0, 0],
@@ -24,21 +35,33 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 
 
 cNumSet = [1, 2, 3, 4, 5, 6, 7, 8,9]
+
+def cloneGrid(grid):
+    r =[]
+    for row in grid:
+        nRow = [c for c in row]
+        r.append(nRow)
+    return r
+
+
 def checkUnique(row) -> bool:
     numSet = set([])
 
     for num in row:
         if num in numSet:
-            print("repeated values")
-            print("\"", row, "\"")
+            # print("repeated values")
+            # print("\"", row, "\"")
             return False
         numSet.add(num)
 
     for i in range(1, 10):
         if i not in numSet:
-            print("missing value")
-            print("\"", row, "\"")
-            print(i)
+            # print("missing value")
+            # print("\"", row, "\"")
+            # print(i)
+            return False
+
+    return True
 
 
 def isValid(grid) -> bool:
@@ -53,17 +76,17 @@ def isValid(grid) -> bool:
             print("\"", row, "\"")
             return False
 
-    print("checking rows...")
+    # print("checking rows...")
     for row in grid:
       if(checkUnique(row) == False):
           return False
 
-    print("checking cols...")
+    # print("checking cols...")
     for row in rotateGrid(grid):
       if(checkUnique(row) == False):
           return False
 
-    print("checking sections...")
+    # print("checking sections...")
     for row in unboxGrid(grid):
       if(checkUnique(row) == False):
           return False
@@ -76,7 +99,6 @@ def rotateGrid(grid):
     for i in range(9):
         rGrid.append([x[i] for x in grid])
     return rGrid
-
 
 def unboxGrid(grid):
     rGrid = [
@@ -97,7 +119,6 @@ def unboxGrid(grid):
             rowIndex = secX+3*secY
             rGrid[rowIndex].append(cell)
     return rGrid
-
 
 def printSudoku(grid):
     vline = '_______________________________'
@@ -130,21 +151,36 @@ def solveGrid(grid):
     for y, row in enumerate(grid):
       secY = (y//3)
       for x, cell in enumerate(row):
-        
         if cell == 0:
-            candidate = possibleVal(grid[y],cNumSet)
-            candidate = possibleVal(rGrid[x],candidate)
+            candidates = possibleVal(grid[y],cNumSet)
+            candidates = possibleVal(rGrid[x],candidates)
             secX = (x//3)
             boxndex = secX+3*secY
-            candidate = possibleVal(uGrid[boxndex],candidate)
+            candidates = possibleVal(uGrid[boxndex],candidates)
+            if len(candidates) == 0: return False
 
-            print(candidate)
-        
-        # grid[y][x] = 1
+            cGrid = cloneGrid(grid)
+
+            for c in candidates:
+                cGrid[y][x] = c
+                printSudoku(cGrid)
+                print()
+                if(isValid(cGrid)) : return cGrid
+                next = solveGrid(cGrid)
+
+                if(next) :return next
+
+    return False
 
 
 # printSudoku(testGrid2)
 # printSudoku(rotateGrid(testGrid2))
 # printSudoku(unboxGrid(testGrid2))
-solveGrid(grid)
+
 printSudoku(grid)
+cGrid = solveGrid(grid)
+printSudoku(cGrid)
+
+printSudoku(testGrid)
+cGrid = solveGrid(testGrid)
+printSudoku(cGrid)
